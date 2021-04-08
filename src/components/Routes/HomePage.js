2d5d0d653022ad38ac267cac4acda41b2738e19b
Christine <huangc909@gmail.com>
2021-04-08 11:36:54 -0400

    Redirect signup to homepage with buttons to create new life section

--- src/components/Routes/HomePage.js
+++ src/components/Routes/HomePage.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import axios from 'axios'
+import apiUrl from './../../apiConfig'
 
 const HomePage = (props) => {
+  const { msgAlert, user } = props
+  const [lifeSections, setLifeSections] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios({
+      url: `${apiUrl}/lifeSections`,
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+      .then(res => {
+        setLifeSections(res.data.lifeSections)
+        setLoading(false)
+      })
+      .then(() => msgAlert({
+        heading: 'Showing all Life Sections',
+        variant: 'success'
+      }))
+      .catch(error => {
+        setLifeSections([])
+        msgAlert({
+          heading: 'Failed to show all life sections' + error.message,
+          variant: 'danger'
+        })
+      })
+  }, [])
+  console.log(lifeSections)
+  const lifeSectionJsx = lifeSections.map(lifeSection => (
+    <div key={lifeSection._id}>
+      <button>
+        <p>{lifeSection.name}</p>
+      </button>
+    </div>
+  ))
+  const startOptions = () => {
+    if (lifeSections.length === 0) {
+      return true
+    }
+  }
+
+  const lifeSectionButtons = (
+    <div>
+      <button>Finance</button>
+      <button>Exercise</button>
+      <button>Work</button>
+      <button>Productivity</button>
+      <button>Spending</button>
+      <button>Food</button>
+      <button>Hobbies</button>
+      <button>Sleep</button>
+    </div>
+  )
+
   return (
     <div>
-      <p>You&apos;ve reached the home page</p>
+      <h1>Life Tracker</h1>
+      <p>Start keeping track of things you&apos;re spending time on in your life</p>
+      <div>{loading ? <p>Loading...</p> : lifeSectionJsx}</div>
+      <div>{startOptions ? lifeSectionButtons : <div></div>}</div>
+      <button>Create New</button>
     </div>
   )
 }
